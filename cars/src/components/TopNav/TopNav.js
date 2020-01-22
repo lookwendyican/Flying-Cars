67f8960678aa553d1769343f7f98dc7336e9661b
lookwendyican <wendyalers@gmail.com>
2020-01-22 18:00:20 -0500

    added axios and react-router-dom

--- cars/src/components/TopNav/TopNav.js
+++ cars/src/components/TopNav/TopNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TopNav";
 import {
   Collapse,
@@ -14,14 +14,43 @@ import {
   DropdownItem,
   NavbarText
 } from "reactstrap";
+const TopNav = props => {
+  const [isOpen, setIsOpen] = useState(false);
 
-class TopNav extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>TopNav</h1>
-      </div>
-    );
-  }
-}
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
 export default TopNav;
